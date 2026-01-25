3a1be3e9b8142cb566cc4f035fb33e5caa48c12b
saisuryavinay <saisuryavinay06@gmail.com>
2026-01-25 14:55:20 +0530

    All Done

--- Backend/server.js
+++ Backend/server.js
@@ -14,6 +14,11 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send("Yummify Backend is Live 🚀");
+});
+
+
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
